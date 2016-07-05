dbf4240b049e0300f786c305e247dc7387ed797b
hurumeki <hurumeki1987@gmail.com>
2016-07-05 22:37:27 +0900

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -44,17 +44,64 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var print = __webpack_require__(1);
-	print("Hello webpack");
+	__webpack_require__(1);
+	(function webpackMissingModule() { throw new Error("Cannot find module \"C:\\Users\\USER\\Data\\git\\es6-notes\""); }());
 
 
 /***/ },
 /* 1 */
 /***/ function(module, exports) {
 
-	module.exports = function(msg) {
-	  document.write("[print] " + msg);
-	};
+	function appendScriptCode(s) {
+	  fetch(s.dataset.url)
+	    .then(request => request.text())
+	    .then(text => {
+	      let code = `
+	\`\`\` js
+	${text}
+	\`\`\`
+	`;
+	      s.parentElement.insertBefore(document.createTextNode(code), s.nextSibling);
+	      s.parentElement.removeChild(s);
+	    });
+	}
+
+
+	function appendScriptResult(elem) {
+	  let text = elem.innerText,
+	    ifr = document.createElement('iframe'),
+	    result,
+	    p;
+	  document.body.appendChild(ifr);
+	  try {
+	    result = ifr.contentWindow.eval(text);
+	  } catch(e) {
+	    result = e;
+	  }
+
+	  result = `
+	> ${result}
+	`;
+	  p = document.createElement('p');
+	  p.appendChild(document.createTextNode(result));
+	  elem.parentElement.parentElement.insertBefore(p, elem.parentElement.nextSibling);
+	  document.body.removeChild(ifr);
+	}
+
+	document.addEventListener('readystatechange', () => {
+	  let scripts = document.querySelectorAll("[data-url]");
+	  Promise.all(Array.from(scripts).map(appendScriptCode))
+	    .then(() => {
+	        let markDeep = document.createElement('script');
+	        markDeep.setAttribute('src','vendor/markdeep.min.js');
+	        document.body.appendChild(markDeep);
+
+	        setTimeout(() => {
+	          let codes = document.querySelectorAll("code");
+	          Array.from(codes).map(appendScriptResult)
+	        }, 1000)
+	    });
+	});
 
 
 /***/ }
